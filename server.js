075c27222d3ec1ed21be302114776329e5df0f27
Ashley Williams <ashley666ashley@gmail.com>
2016-09-14 20:26:55 +0200

    create a server that listens at 127.0.0.1:8080

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 // server.js
 var express = require('express');
+var host = '127.0.0.1';
+var port = '8080';
+
+var app = express();
+
+app.listen(port, host);
+console.log("Server started on 127.0.0.1:8080...");
